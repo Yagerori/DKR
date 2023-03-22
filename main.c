2977709ff49429cc9d70f0512916797426f5259e
Yagerori <nastya160903@gmail.com>
2023-03-22 20:14:30 +0300

    Merge branch 'Ivanova'
    Conflicts:
    	main.c

--- main.c
+++ main.c
@@ -7,7 +7,6 @@ int main(int argc, char **argv)
 
     printf("Enter your first name: ");
     fgets(first, 255, stdin);
-    name[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
 
     printf("Hello, %s!\n", name);
